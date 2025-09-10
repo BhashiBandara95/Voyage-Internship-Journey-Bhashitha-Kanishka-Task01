34467b16c63c80cf2f145dd879ae996262fbcce6
BhashiBandara95 <ugabkanishka@gmail.com>
2025-09-11 01:57:30 +0530

    Understand ReactJS Event Handling Arrow Functions, Anonymous Function, Event Object

--- src/CodeBlocks/Code11.jsx
+++ src/CodeBlocks/Code11.jsx
@@ -2,27 +2,25 @@ import HeaderContent from "./Components/HeaderContent/HeaderContent";
 
 function App() {
 	// Normal Function
-	function hello1(e) {
-		// event object
-		console.log("Hello 1", e.target);
+	function hello1() {
+		console.log("Hello 1");
 	}
 
 	// Arrow function
 
-	const hello2 = (e) => {
-		// event object
-		console.log("Hello 2", e);
+	const hello2 = () => {
+		console.log("Hello 2");
 	};
 	// parameterized function
 
-	function hello3(name, e) {
-		console.log("Hello 3 " + name, e);
+	function hello3(name) {
+		console.log("Hello 3 " + name);
 	}
 
 	// Arrow function
 
-	const hello4 = (name, e) => {
-		console.log("Hello 4 " + name, e);
+	const hello4 = (name) => {
+		console.log("Hello 4 " + name);
 	};
 
 	return (
@@ -30,18 +28,16 @@ function App() {
 			<div id="wrapper">
 				<button onClick={hello1}>Click1</button>
 				<button onClick={hello2}>Click2</button>
-
 				<button
-					onClick={(e) => {
-						// access to event object
-						hello3("Bhashitha", e.target);
+					onClick={() => {
+						hello3("Bhashitha");
 					}} // wrap from anonymous function
 				>
 					Click3
 				</button>
 				<button
-					onClick={(e) => {
-						hello4("Kasun", e);
+					onClick={() => {
+						hello4("Kasun");
 					}}
 				>
 					Click4
